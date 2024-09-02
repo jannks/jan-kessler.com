3d42bfce966abf2bf593ae5cd875eb61d029892e
jannks <jan.kessler@ah-oh.com>
2024-09-02 22:00:50 +0200

    add seo description

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jan-kessler.com',
     output: 'server',
     adapter: node({
         mode: 'standalone',
     }),
+    integrations: [sitemap({})],
 });
